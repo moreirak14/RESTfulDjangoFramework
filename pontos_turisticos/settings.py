b982a491e64a0f0e0b7287b27c0970619710e923
moreirak14 <kaiquemoreiracloud@gmail.com>
2021-11-12 10:22:33 -0300

    Adicionando paginação na app por tamanho de registros

--- pontos_turisticos/settings.py
+++ pontos_turisticos/settings.py
@@ -134,7 +134,11 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = 'imagens'
 
 REST_FRAMEWORK = {
-    'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',)
+    'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',),
+
+    # Paginação de busca por tamanho
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 2
 }
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
