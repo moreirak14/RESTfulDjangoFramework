106870df504bce87c6037805920e3f4df5782f1a
moreirak14 <kaiquemoreiracloud@gmail.com>
2021-11-12 10:36:14 -0300

    Ação de aprovado e reprovado na app comentarios

---
+++ comentarios/action.py
@@ -0,0 +1,12 @@
+# Aprovação em massa
+
+def reprova_comentarios(modeladmin, request, queryset):
+    queryset.update(aprovado=False)
+
+
+def aprova_comentarios(modeladmin, request, queryset):
+    queryset.update(aprovado=True)
+
+
+reprova_comentarios.short_description = 'Reprova comentarios'
+aprova_comentarios.short_description = 'Aprova comentarios'
--- comentarios/admin.py
+++ comentarios/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from .models import Comentario
+from .action import reprova_comentarios, aprova_comentarios
+
+
+class ComentarioAdmin(admin.ModelAdmin):
+    list_display = ['usuario', 'data', 'aprovado']
+    actions = [reprova_comentarios, aprova_comentarios]
 
 
 # Register your models here.
-admin.site.register(Comentario)
+admin.site.register(Comentario, ComentarioAdmin)
